8f01e8639eae5c08459912e03b583a93a87683d0
felix <felix.martel@polytechnique.edu>
2020-10-05 11:43:06 +0200

    Tensor initialization (identity tensor + random one-hot tensor)

--- utils/tensor.py
+++ utils/tensor.py
@@ -3,6 +3,16 @@ from scipy import sparse
 import numpy as np
 
 
+def sparse_eye(dim: int) -> torch.sparse.FloatTensor:
+    """
+    Make a sparse identity matrix of dimension `dim`.
+    """
+    return torch.sparse.FloatTensor(
+        torch.arange(dim).repeat(2, 1),
+        torch.ones(dim),
+        torch.Size([dim, dim])
+    )
+
 def normalize(a):
     """
     Normalize an adjacency matrix, as described in Kipf et al. 2017
@@ -27,3 +37,8 @@ def csr_to_torch(csr: sparse.csr_matrix) -> torch.sparse.LongTensor:
         torch.Size(coo.shape)
     )
     return t
+
+def make_onehot(n_samples, n_classes):
+    y = torch.zeros(n_samples, n_classes)
+    y[torch.arange(n_samples),torch.randint(0, n_classes, (n_samples,))] = 1.
+    return y
