80e4297c840106fb0c2d7cb6542cf65321fe759a
felix <felix.martel@polytechnique.edu>
2020-10-05 11:42:21 +0200

    Add normalization function (from A to D^-1*A in Kipf's notations)

--- utils/tensor.py
+++ utils/tensor.py
@@ -3,6 +3,17 @@ from scipy import sparse
 import numpy as np
 
 
+def normalize(a):
+    """
+    Normalize an adjacency matrix, as described in Kipf et al. 2017
+    :param a: a ndarray representing an adjacency matrix
+    :return: a normalized ndarray, with the same shape as `a`
+    """
+    d = np.array(a.sum(1)).squeeze()
+    d = np.divide(1, d, where=d!=0)
+    d = sparse.diags(d, format="csr")
+    return d * a
+
 def csr_to_torch(csr: sparse.csr_matrix) -> torch.sparse.LongTensor:
     """
     Transform a `scipy.sparse.csr_matrix` to a sparse Torch tensor.
